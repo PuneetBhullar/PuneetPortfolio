fd9b96858925d4dc6ee7271aa10b8254b5164ccd
PuneetBhullar <puneet.mia@gmail.com>
2023-05-09 22:07:29 -0400

    Front page made.

--- puneetportfolio/src/App.js
+++ puneetportfolio/src/App.js
@@ -1,10 +1,130 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 export default function App() {
+  const [openTab, setOpenTab] = React.useState(1);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+  <div className="flex flex-wrap py-2 place-content-center">
+  <div className="w-full px-4">
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-blueGray-500 rounded">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+          <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#pablo">
+            Puneet Bhullar
+          </a>
+          <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+            <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+          </button>
+        </div>
+        <div className="flex lg:flex-grow items-center" id="example-navbar-info">
+          <ul className="flex flex-col lg:flex-row list-none ml-auto">
+              <li className="nav-item">
+              <a
+                className={
+                  "px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 " +
+                  (openTab === 1
+                    ? "text-white bg-blueGray-600"
+                    : "text-sky-50 bg-blueGray-600")
+                }
+                onClick={e => {
+                  e.preventDefault();
+                  setOpenTab(1);
+                }}
+                data-toggle="tab"
+                href="#link1"
+                role="tablist"
+              >
+                Profile
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className={
+                  "px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 " +
+                  (openTab === 2
+                    ? "text-white bg-blueGray-600"
+                    : "text-white bg-blueGray-600")
+                }
+                onClick={e => {
+                  e.preventDefault();
+                  setOpenTab(2);
+                }}
+                data-toggle="tab"
+                href="#link2"
+                role="tablist"
+              >
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className={
+                  "px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 " +
+                  (openTab === 3
+                    ? "text-white bg-blueGray-600"
+                    : "text-sky-50 bg-blueGray-600")
+                }
+                onClick={e => {
+                  e.preventDefault();
+                  setOpenTab(3);
+                }}
+                data-toggle="tab"
+                href="#link3"
+                role="tablist"
+              >
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div>
+<div className="relative flex flex-col min-w-0 break-words">
+            <div className="px-4 py-5 flex-auto">
+              <div className="tab-content tab-space">
+                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+                <div className="flex flex-wrap justify-center">
+                  <div className="mt-8">
+                    <img src="profile.png" alt="Puneet Bhullar" className="shadow-lg rounded-full w-60 h-60 align-middle border-none" />
+                  </div>
+                </div>
+                <div className="flex flex-wrap justify-center mt-4">
+                    <a className='text-white font-semibold text-3xl'>Hello there! My name is Puneet Bhullar.</a>
+                </div>
+                <div className="flex flex-wrap justify-center mt-4">
+                    <a className='text-white text-lg'>Hello there! My name is Puneet Bhullar.</a>
+                </div>
+                </div>
+                <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+                  <p>
+                    Completely synergize resource taxing relationships via
+                    premier niche markets. Professionally cultivate one-to-one
+                    customer service with robust ideas.
+                    <br />
+                    <br />
+                    Dynamically innovate resource-leveling customer service for
+                    state of the art customer service.
+                  </p>
+                </div>
+                <div className={openTab === 3 ? "block" : "hidden"} id="link3">
+                  <p>
+                    Efficiently unleash cross-media information without
+                    cross-media value. Quickly maximize timely deliverables for
+                    real-time schemas.
+                    <br />
+                    <br /> Dramatically maintain clicks-and-mortar solutions
+                    without functional solutions.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+    </>
   )
 }
